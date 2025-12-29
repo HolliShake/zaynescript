b523cde64611463b8646c717b1d13011c0e4e1df
HolliShake <redondophilippandrewroa.dev@gmail.com>
2025-12-28 21:42:34 -0800

    Refactor factorial function: Improve readability by adding braces to the if statement in the fact function.

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ cls
 
 if exist main.exe del main.exe
 
-gcc -g -O3 -Wno-pointer-sign main.c src/*.c utf/*.c utf/utf8proc/*.c -o main.exe -lm -ldl -lpthread
+gcc -g -O3 -Wno-pointer-sign main.c src/*.c utf/*.c utf/utf8proc/*.c -o main.exe -static -lm -ldl -lpthread
 chcp 65001 >nul
 .\main.exe
 pause
