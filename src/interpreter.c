69584b53d460f232b4e9957b550fdb73110274e9
HolliShake <redondophilippandrew.dev@gmail.com>
2026-04-01 19:35:06 +0800

    refactor: Remove unused stdio.h include from interpreter.c

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,7 +1,5 @@
 #include "./interpreter.h"
 
-#include <stdio.h>
-
 static void* interpreter_bf_realloc(void* opaque, void* ptr, size_t size) {
 	// libbf uses size == 0 to signal a free() operation
 	if (size == 0) {
