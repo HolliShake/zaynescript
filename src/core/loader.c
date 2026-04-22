702e86233a8fa03cbb65c7e905497169965a90b9
HolliShake <redondophilippandrew.dev@gmail.com>
2026-04-23 05:11:52 +0800

    Regex feature

--- src/core/loader.c
+++ src/core/loader.c
@@ -1,4 +1,3 @@
-
 #include "./loader.h"
 
 CoreMapper _CoreModuleMappers[] = {
---
+++ src/core/regex.h
@@ -0,0 +1,10 @@
+#include "../../libregex/libunicode.h"
+#include "../global.h"
+#include "../hashmap.h"
+#include "../value.h"
+
+#ifndef CORE_REGEX_H
+#define CORE_REGEX_H
+
+
+#endif
